3f77911824afaa9263a2798abd6783449f3ecc04
Robin Martinjak <rob@rmartinjak.de>
2014-02-18 14:36:35 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='pyopenmensa',
+      version='0.0',
+      description="Python wrapper and helper for openmensa api and feed",
+      author="Malte Swart",
+      url="http://pyom.devtation.de/",
+      packages=['pyopenmensa'],
+      )
